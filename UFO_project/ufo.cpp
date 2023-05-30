1e180752dbe63b0fbc486734cf477c19512e17de
Stephen Irinoye <123007810+Stephen-I@users.noreply.github.com>
2023-05-30 14:30:54 +0100

    Add files via upload

---
+++ UFO_project/ufo.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "ufo_functions.hpp"
+
+int main() {
+std::string codeword = "codecademy";
+std::string answer = "__________";
+int misses = 0;
+std::vector<char> incorrect;
+bool guess = false;
+char letter;
+
+greet();
+
+while(answer != codeword && misses < 7){
+  display_misses(misses);
+  display_status(incorrect, answer);
+  std::cout << "Please enter your guess: \n";
+  std::cin >> letter;
+
+  for(int i = 0; i < codeword.size(); i++){
+    if(letter == codeword[i]){
+      answer[i] == letter;
+      guess = true;
+    }
+    
+  }
+   if(guess){
+      std::cout << "\nCorrect! You're closer to cracking the codeword.\n";
+    } else {
+      std::cout << "\nIncorrect! The tractor beam pulls the person in further.\n";
+      incorrect.push_back(letter);
+      misses++;
+    }
+    guess = false;
+}
+end_game(answer, codeword);
+}
